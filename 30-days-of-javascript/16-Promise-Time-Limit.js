7e38e2550ddbfde88a0ae3091bffd75520615d5a
Divyanka Pagare <113618065+pagaredivyanka@users.noreply.github.com>
2025-03-12 12:28:35 +0530

    Delete 30-days-of-javascript directory

--- 30-days-of-javascript/16-Promise-Time-Limit.js
+++
@@ -1,9 +0,0 @@
-var timeLimit = function(fn, t) {
-  return async function(...args) {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {reject("Time Limit Exceeded")}, t)
-
-      fn(...args).then(resolve).catch(reject)
-    })
-  }
-};
